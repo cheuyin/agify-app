15c055eb0523e82ab9922dc7a8cafd6aa7cd1e41
Yin <yinstanleycheung@gmail.com>
2022-06-26 15:17:23 -0700

    Enable basic form submission

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log("Server running on port " + port);
