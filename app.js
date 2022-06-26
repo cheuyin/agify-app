168c8fa08e988c03f339a98a7cc5dead896f860c
Yin <yinstanleycheung@gmail.com>
2022-06-26 16:14:16 -0700

    Add next step comment

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", (req, res) => {
-  // getAPIData(req.body)
-  //   .then(data => displayData(data));
-  getAPIData(req.body);
+  getAPIData(req.body)
+    .then(data => displayData(data));
 })
 
 app.listen(port, () => {
@@ -24,14 +23,25 @@ app.listen(port, () => {
 function getAPIData(query) {
   const name = query.name;
   const countryCode = query.country_code;
-  const reqURL = `https://api.agify.io?name=${name}` +  
-  ((countryCode === "all") ? "" : `&country_id=${countryCode}`);
-  axios
+  const reqURL = `https://api.agify.io?name=${name}` +
+    ((countryCode === "all") ? "" : `&country_id=${countryCode}`);
+  return axios
     .get(reqURL)
-    .then(response => {
-      console.log(response);
+    .catch(error => {
+      console.log(error)
     })
 }
 
 function displayData(apiData) {
+  if (apiData.statusCode !== "200") {
+    showErrorPage(apiData);
+  }
 }
+
+
+function showErrorPage(apiData) {
+  console.log("Error!");
+}
+
+// TODO: ENABLE WRITING CONTENT TO HTML VIA JS
+
