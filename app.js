9c9a811853e400406637c3886292bb3112c40513
Yin <yinstanleycheung@gmail.com>
2022-06-28 14:45:39 -0700

    Install EJS package

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 })
 
 app.post("/", (req, res) => {
